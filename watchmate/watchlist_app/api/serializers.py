1489786b46c4b1be8f4085c69e113bcfead28a87
basavraj0909 <basavrajn123@gmail.com>
2024-05-02 19:35:43 +0530

    branch-validations: Validators

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -3,9 +3,17 @@ from rest_framework import serializers
 from watchlist_app.models import Movie
 
 
+
+def name_lenght(value):
+    "validators"
+    if len(value) < 2:
+        raise serializers.ValidationError('Name is too short')
+    return value
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators=[name_lenght]) # for validators
+    # name = serializers.CharField()
     description = serializers.CharField()
     active = serializers.BooleanField()
 
@@ -18,3 +26,17 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active',instance.active)
         instance.save()
         return instance
+
+    # def validate_name(self, value):
+    #     """below is field level validation"""
+    #     if len(value) < 2:
+    #         raise serializers.ValidationError('Name is too short')
+    #     else:
+    #         return value
+
+    def validate(self, data):
+        """below is Object level validation"""
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("Title and Description should be different")
+
+        return data

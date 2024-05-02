0cc60ffc67740c03a334ed6abb0f7af4743fb114
basavraj0909 <60147779+basavraj0909@users.noreply.github.com>
2024-05-02 17:34:02 +0530

    branch-cbv:Function Based View with Model Serializer

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -2,11 +2,15 @@ from django.urls import path, include
 
 # from watchmate import watchlist_app
 """CLASS BASED VIEW"""
-
+from .views import MovielistView, MovieDetailView
+urlpatterns = [
+    path('list/', MovielistView.as_view(), name='movie-list'),
+    path('<int:pk>', MovieDetailView.as_view(), name='movie-detail'),
+]
 
 """FUNCTION BASED VIEW"""
 # from .views import movie_list, movie_detail
-urlpatterns = [
+# urlpatterns = [
     # path('list/', movie_list, name='movie-list'),
     # path('<int:pk>', movie_detail, name='movie-detail'),
-]
+# ]

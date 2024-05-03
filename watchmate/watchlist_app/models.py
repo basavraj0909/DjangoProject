81838140b69524a1a9b16e8526aa75384aa753b4
basavraj0909 <basavrajn123@gmail.com>
2024-05-03 08:37:09 +0530

    Serializers and model serializers, validators

--- watchmate/watchlist_app/models.py
+++ watchmate/watchlist_app/models.py
@@ -2,12 +2,24 @@ from django.db import models
 
 # Create your models here.
 
-class Movie(models.Model):
+class StreamPlatform(models.Model):
     name = models.CharField(max_length=30)
-    description = models.CharField(max_length=200)
-    active = models.BooleanField(default=True)
+    about = models.CharField(max_length=150)
+    website = models.URLField(max_length=100)
 
 
-    # def __str__(self):
-    #     return self.name
+    def __str__(self):
+        return self.name
+
+
+class Watchlist(models.Model):
+    title = models.CharField(max_length=30)
+    storyline = models.CharField(max_length=200)
+    platform = models.ForeignKey(StreamPlatform,on_delete=models.CASCADE, related_name='watchlist')
+    active = models.BooleanField(default=True) # if the content is release or launched
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
 
